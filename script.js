da24312875e5689b890f5f2856ead80ce7327ec1
Manaregr8 <99804338+Manaregr8@users.noreply.github.com>
2023-06-27 22:08:04 +0530

    added files via upload
    uploaded whole site

---
+++ script.js
@@ -0,0 +1,37 @@
+
+function mainSection() {
+    return(
+        
+    );
+}
+
+function Header() {
+    return (
+    
+    );
+}
+
+function Footer() {
+    return (
+    
+    );
+}
+
+function MainContent() {
+    return (
+    
+    );
+}
+
+function Page() {
+    return (
+        <div>
+            <NavBar />
+            <Header />
+            <MainContent />
+            <Footer />
+        </div>
+    );
+}
+
+ReactDOM.render(<Page />, document.getElementById("root"));
